3dd6b0a41c43da03c9322844ea953a08daca2066
Jérôme Mahuet <jerome.mahuet@gmail.com>
2015-12-01 14:51:55 +0100

    lil changes

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,14 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as B
+import qualified Data.HashMap as M
 import Lib
 
 main :: IO ()
 main = do
   fileStr <- B.readFile "resources/sirens_fxt.txt"
   let sirens = parseSirens fileStr
-  putStrLn "Number of Sirens which are present only twice: "
-  print $ length $ filterSirensByNumber 2 sirens
-  putStrLn "Sirens which are present only once: "
-  print $ length $ filterSirensByNumber 1 sirens
+  putStrLn "Number of Sirens which are present twice or more: "
+  print $ length $ M.keys . M.filter (>=2) $ sirens
+  putStrLn "Number of Sirens which are present only once: "
+  print $ length $ M.keys . M.filter (==1) $ sirens
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,6 @@
 
 module Lib
     ( parseSirens
-    , filterSirensByNumber
     ) where
 
 import qualified Data.HashMap as M
@@ -15,7 +14,3 @@ type Sirens = M.Map B.ByteString Int
 -- | Build a hashmap by taking the file as bytestring
 parseSirens :: B.ByteString -> Sirens
 parseSirens = foldl (\ z bs -> M.insertWith (+) bs 1 z) M.empty . B.lines
-
--- | Only get the keys of a hashmap that has n occurences
-filterSirensByNumber :: Int -> Sirens -> [B.ByteString]
-filterSirensByNumber n = M.keys . M.filter (==n)
