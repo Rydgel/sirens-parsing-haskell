b8d396b9027d025a779b17f2c3b4707aba3517d2
Jérôme Mahuet <jerome.mahuet@gmail.com>
2015-12-01 01:47:23 +0100

    optim

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,8 @@ import Lib
 main :: IO ()
 main = do
   fileStr <- B.readFile "resources/sirens_fxt.txt"
+  let sirens = parseSirens fileStr
   putStrLn "Number of Sirens which are present only twice: "
-  print $ length $ filterSirensByNumber 2 $ parseSirens fileStr
+  print $ length $ filterSirensByNumber 2 sirens
   putStrLn "Sirens which are present only once: "
-  print $ length $ filterSirensByNumber 1 $ parseSirens fileStr
+  print $ length $ filterSirensByNumber 1 sirens
