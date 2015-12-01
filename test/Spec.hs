f6d08361626c9b68d072fa599362e0139f554e13
Jérôme Mahuet <jerome.mahuet@gmail.com>
2015-12-01 20:37:33 +0100

    readme

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,12 +15,23 @@ main = do
     describe "Verify that a non existent Sirens" $
       it "is not found" $
         M.lookup "9999999999999" sirens `shouldBe` Nothing
+
     describe "Verify that an existent Sirens" $
       it "is found with 1 occurrence" $
         M.lookup "005520242" sirens `shouldBe` Just 1
+
     describe "Verify that an existent Sirens" $
       it "is found with 2 occurrences" $
         M.lookup "005541552" sirens `shouldBe` Just 2
+
     describe "Verify that an existent Sirens" $
       it "is found with 32 occurrences" $
         M.lookup "177501517" sirens `shouldBe` Just 32
+
+    describe "Number of Sirens which are present twice or more" $
+      it "should be 2256" $
+        (M.size . M.filter (>=2)) sirens `shouldBe` 2256
+
+    describe "Number of Sirens which are present only once" $
+      it "should be 77445" $
+        (M.size . M.filter (==1)) sirens `shouldBe` 77445
